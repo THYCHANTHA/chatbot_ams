3685d45c66c14d147c6aa37ab0202126bd750e13
Thy Chantha <157097678+THYCHANTHA@users.noreply.github.com>
2025-06-26 03:48:17 +0700

    Update chatbot_gemini.py

--- chatbot_gemini.py
+++ chatbot_gemini.py
@@ -17,6 +17,9 @@ genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 st.set_page_config(page_title="AMS Chatbot", layout="wide")
 st.title("AMS Chatbot")
 
+# st.write("Current working directory:", os.getcwd())
+files_in_dir = os.listdir()  # keep this line if you still need the list
+# st.write("Files in directory:", files_in_dir)
 
 required_files = [
     "students.csv",
